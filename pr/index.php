25547555917312857fd1625991f0aba735f13755
adikgh <adiikgh@gmail.com>
2024-08-12 15:38:51 +0500

    3.00039

--- pr/index.php
+++ pr/index.php
@@ -111,10 +111,13 @@
                   </ul>
                   <div class="blo8_cip">
                      <div class="blo8_cipo">
-                        <div class="blo8_cipoi">26 000 тенге</div>
+                        <div class="blo8_cipoi blo8_cipoi_gg">55 000 тенге</div>
+                     </div>
+                     <div class="blo8_cipo">
+                        <div class="blo8_cipoi">40 000 тенге</div>
                      </div>
                      <div class="blo8_cip_btn">
-                        <div class="btn btn_ukl" data-price="26 000" data-price2="0">Төлем жасаймын</div>
+                        <div class="btn btn_ukl" data-price="40 000" data-price2="0">Төлем жасаймын</div>
                      </div>
                   </div>
                </div>
