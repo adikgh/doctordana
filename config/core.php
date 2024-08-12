506c23a1e1e9f74bf2417b1010ce787b60123c43
adikgh <adiikgh@gmail.com>
2024-08-12 20:49:39 +0500

    3.1275

--- config/core.php
+++ config/core.php
@@ -82,7 +82,7 @@
 
 
     // setting
-    $ver = 1.126;
+    $ver = 1.127;
     $site = mysqli_fetch_array(db::query("select * from `site` where id = 1"));
     $site_set = [
         'header' => true,
